549dc6500c264d10aa7352b84d6b94e6c6bea78b
MikeYuanMY <yuan.mike84@gmail.com>
2020-01-07 20:52:24 -0800

    code reformat

--- imports/ui/pages/Login.vue
+++ imports/ui/pages/Login.vue
@@ -1,141 +1,143 @@
 <template>
-  <div class="login">
-    <h1>Login Page</h1>
-    <div class="content overflow-hidden">
-      <div class="row">
-        <div class="col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 col-lg-4 col-lg-offset-4">
-          <!-- Login Block -->
-          <div class="block block-themed animated fadeIn">
-            <div class="block-header bg-primary">
-              <ul class="block-options">
-                <li>
-                  <a href="base_pages_reminder.html">Forgot Password?</a>
-                </li>
-                <li>
-                  <a
-                    href="base_pages_register.html"
-                    data-toggle="tooltip"
-                    data-placement="left"
-                    title="New Account"
-                  >
-                    <i class="fa fa-key"></i>
-                  </a>
-                </li>
-              </ul>
-              <h3 class="block-title">Login</h3>
-            </div>
-            <div class="block-content block-content-full block-content-narrow">
-              <!-- Login Title -->
-              <h1 class="h2 font-w600 push-30-t push-5">TeamMemberApp</h1>
-              <p>Welcome, please login.</p>
-              <!-- END Login Title -->
+    <div class="login">
+        <h1>Login Page</h1>
+        <div class="content overflow-hidden">
+            <div class="row">
+                <div class="col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 col-lg-4 col-lg-offset-4">
+                    <!-- Login Block -->
+                    <div class="block block-themed animated fadeIn">
+                        <div class="block-header bg-primary">
+                            <ul class="block-options">
+                                <li>
+                                    <a href="base_pages_reminder.html">Forgot Password?</a>
+                                </li>
+                                <li>
+                                    <a
+                                            href="base_pages_register.html"
+                                            data-toggle="tooltip"
+                                            data-placement="left"
+                                            title="New Account"
+                                    >
+                                        <i class="fa fa-key"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                            <h3 class="block-title">Login</h3>
+                        </div>
+                        <div class="block-content block-content-full block-content-narrow">
+                            <!-- Login Title -->
+                            <h1 class="h2 font-w600 push-30-t push-5">TeamMemberApp</h1>
+                            <p>Welcome, please login.</p>
+                            <!-- END Login Title -->
 
-              <!-- Login Form -->
-              <!-- jQuery Validation (.js-validation-login class is initialized in js/pages/base_pages_login.js) -->
-              <!-- For more examples you can check out https://github.com/jzaefferer/jquery-validation -->
-              <form
-                class="js-validation-login form-horizontal push-30-t push-50"
-                action="index.html"
-                method="post"
-              >
-                <div class="form-group">
-                  <div class="col-xs-12">
-                    <div class="form-material form-material-primary">
-                      <input
-                        class="form-control"
-                        type="text"
-                        id="login-username"
-                        name="login-username"
-                        v-model="loginInfo.userName"
-                      />
-                      <label for="login-username">Username</label>
+                            <!-- Login Form -->
+                            <!-- jQuery Validation (.js-validation-login class is initialized in js/pages/base_pages_login.js) -->
+                            <!-- For more examples you can check out https://github.com/jzaefferer/jquery-validation -->
+                            <form
+                                    class="js-validation-login form-horizontal push-30-t push-50"
+                                    action="index.html"
+                                    method="post"
+                            >
+                                <div class="form-group">
+                                    <div class="col-xs-12">
+                                        <div class="form-material form-material-primary">
+                                            <input
+                                                    class="form-control"
+                                                    type="text"
+                                                    id="login-username"
+                                                    name="login-username"
+                                                    v-model="loginInfo.userName"
+                                            />
+                                            <label for="login-username">Username</label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="col-xs-12">
+                                        <div class="form-material form-material-primary">
+                                            <input
+                                                    class="form-control"
+                                                    type="password"
+                                                    id="login-password"
+                                                    name="login-password"
+                                                    v-model="loginInfo.password"
+                                            />
+                                            <label for="login-password">Password</label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="col-xs-12">
+                                        <label class="css-input switch switch-sm switch-primary">
+                                            <input type="checkbox" id="login-remember-me" name="login-remember-me"/>
+                                            <span></span> Remember Me?
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="col-xs-12 col-sm-6 col-md-4">
+                                        <button class="btn btn-block btn-primary" type="submit" @click.prevent="login">
+                                            <i class="fa fa-sign-in pull-right"></i> Log in
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                            <!-- END Login Form -->
+                        </div>
                     </div>
-                  </div>
+                    <!-- END Login Block -->
                 </div>
-                <div class="form-group">
-                  <div class="col-xs-12">
-                    <div class="form-material form-material-primary">
-                      <input
-                        class="form-control"
-                        type="password"
-                        id="login-password"
-                        name="login-password"
-                        v-model="loginInfo.password"
-                      />
-                      <label for="login-password">Password</label>
-                    </div>
-                  </div>
-                </div>
-                <div class="form-group">
-                  <div class="col-xs-12">
-                    <label class="css-input switch switch-sm switch-primary">
-                      <input type="checkbox" id="login-remember-me" name="login-remember-me" />
-                      <span></span> Remember Me?
-                    </label>
-                  </div>
-                </div>
-                <div class="form-group">
-                  <div class="col-xs-12 col-sm-6 col-md-4">
-                    <button class="btn btn-block btn-primary" type="submit" @click.prevent="login">
-                      <i class="fa fa-sign-in pull-right"></i> Log in
-                    </button>
-                  </div>
-                </div>
-              </form>
-              <!-- END Login Form -->
             </div>
-          </div>
-          <!-- END Login Block -->
         </div>
-      </div>
-    </div>
-    <!-- END Login Content -->
+        <!-- END Login Content -->
 
-    <!-- Login Footer -->
-    <div class="push-10-t text-center animated fadeInUp">
-      <small class="text-muted font-w600">
-        <span class="js-year-copy"></span> &copy; OneUI 1.3
-      </small>
+        <!-- Login Footer -->
+        <div class="push-10-t text-center animated fadeInUp">
+            <small class="text-muted font-w600">
+                <span class="js-year-copy"></span> &copy; OneUI 1.3
+            </small>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return { loginInfo: {} };
-  },
-  mounted() {
-    // let appScript = document.createElement("script");
-    // appScript.setAttribute("src", "/assets/js/app.js");
-    // document.head.appendChild(appScript);
-  },
-  updated() {
-    // let loginValidationScript = document.createElement("script");
-    // loginValidationScript.setAttribute(
-    //   "src",
-    //   "/assets/js/pages/base_pages_login.js"
-    // );
-    // document.head.appendChild(loginValidationScript);
-  },
-  destroyed() {},
-  methods: {
-    login() {
-      const { userName, password } = this.loginInfo;
-      Meteor.loginWithPassword(userName, password, error => {
-        if (error) {
-          return swal("Login Error", error.reason, "error");
-        } else {
-          this.$router.push({ name: "personal" });
+    export default {
+        data() {
+            return {loginInfo: {}};
+        },
+        mounted() {
+            // let appScript = document.createElement("script");
+            // appScript.setAttribute("src", "/assets/js/app.js");
+            // document.head.appendChild(appScript);
+        },
+        updated() {
+            // let loginValidationScript = document.createElement("script");
+            // loginValidationScript.setAttribute(
+            //   "src",
+            //   "/assets/js/pages/base_pages_login.js"
+            // );
+            // document.head.appendChild(loginValidationScript);
+        },
+        destroyed() {
+        },
+        methods: {
+            login() {
+                console.log("log in");
+                const {userName, password} = this.loginInfo;
+                Meteor.loginWithPassword(userName, password, error => {
+                    if (error) {
+                        return swal("Login Error", error.reason, "error");
+                    } else {
+                        this.$router.push({name: "personal"});
+                    }
+                });
+
+                // uese meteor login with password to log in
+
+                // sweet alert if faile and redirect if success
+            }
         }
-      });
-
-      // uese meteor login with password to log in
-
-      // sweet alert if faile and redirect if success
-    }
-  }
-};
+    };
 </script>
 
 <style></style>
--- imports/ui/pages/Teams.vue
+++ imports/ui/pages/Teams.vue
@@ -2,113 +2,116 @@
     <div>
         <h1>Teams</h1>
         <div class="row">
-             <div class="col-md-12">
+            <div class="col-md-12">
 
-            <div class="block">
-                <div class="block-header">
-                    <h3 class="block-title">Create New Team</h3>
-                </div>
-                <div class="block-content block-content-narrow">
-                    <form class="teamInsertForm form-horizontal push-10-t" action="base_forms_elements_modern.html"
-                        method="post" onsubmit="return false;">
-                        <div class="form-group">
-                            <div class="col-sm-9">
-                                <div class="form-material">
-                                    <input class="form-control" type="text" id="name" name="name" v-model="newTeamInfo.name"
-                                        placeholder="Please enter your team name">
-                                    <label for="name">Team Name</label>
+                <div class="block">
+                    <div class="block-header">
+                        <h3 class="block-title">Create New Team</h3>
+                    </div>
+                    <div class="block-content block-content-narrow">
+                        <form action="base_forms_elements_modern.html" class="teamInsertForm form-horizontal push-10-t"
+                              method="post" onsubmit="return false;">
+                            <div class="form-group">
+                                <div class="col-sm-9">
+                                    <div class="form-material">
+                                        <input class="form-control" id="name" name="name"
+                                               placeholder="Please enter your team name"
+                                               type="text"
+                                               v-model="newTeamInfo.name">
+                                        <label for="name">Team Name</label>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
 
-                        <div class="form-group">
-                            <div class="col-xs-12">
-                                <div class="form-material">
-                                    <textarea class="form-control" id="desc" name="desc" rows="3"
-                                        placeholder="Please add a description for your team" v-model="newTeamInfo.description"></textarea>
-                                    <label for="desc">Description</label>
+                            <div class="form-group">
+                                <div class="col-xs-12">
+                                    <div class="form-material">
+                                    <textarea class="form-control" id="desc" name="desc"
+                                              placeholder="Please add a description for your team"
+                                              rows="3"
+                                              v-model="newTeamInfo.description"/>
+                                        <label for="desc">Description</label>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
 
-                        <div class="form-group">
-                            <div class="col-sm-9">
-                                <button class="btn btn-sm btn-primary" type="submit" @click.prevent="createTeam">Submit</button>
+                            <div class="form-group">
+                                <div class="col-sm-9">
+                                    <button @click.prevent="createTeam" class="btn btn-sm btn-primary" type="submit">
+                                        Submit
+                                    </button>
+                                </div>
                             </div>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
+
             </div>
-
         </div>
-        </div>
-       <div v-if="!$subReady.teams">Loading...</div>
+        <div v-if="!$subReady.teams">Loading...</div>
 
         <ul>
-            <li v-for="team in teams" :key='team._id'><a>{{team.name}} - {{team.descrption}}</a></li>
+            <li :key='team._id' v-for="team in teams"><a>{{team.name}} - {{team.descrption}}</a></li>
         </ul>
-    
+
     </div>
-  
+
 </template>
 
 <script>
-	import { Teams } from "/imports/api/teams/teams.js";
+    import {Teams} from "/imports/api/teams/teams.js";
 
-	export default {
-		data() {
-			return {
-				newTeamInfo: {}
-			};
-		},
-		// meteor: {
-		// 	$subscribe: {
-		// 		"teams": []
-		// 	},
-		// 	teams() {
-		// 		return Teams.find({}, { sort: { createdAt: -1 } }); //newly created first
-		// 	}
+    export default {
+        data() {
+            return {
+                newTeamInfo: {}
+            };
+        },
+        // meteor: {
+        // 	$subscribe: {
+        // 		"teams": []
+        // 	},
+        // 	teams() {
+        // 		return Teams.find({}, { sort: { createdAt: -1 } }); //newly created first
+        // 	}
 
-		created() {
-			this.$subscribe('teams', () => [])
-		},
-		computed: {
-			teams() {
-				return this.$autorun(() => Teams.find({}, { sort: { createdAt: -1 } }))
-			}
-		},
+        created() {
+            this.$subscribe('teams', () => [])
+        },
+        computed: {
+            teams() {
+                return this.$autorun(() => Teams.find({}, {sort: {createdAt: -1}}))
+            }
+        },
 
-		// },
-		// created() {
-		// 	this.$subscribe('teams');
-		// },
-		// computed: {
-		// 	teams() {
-		// 		return this.$autorun(() => Teams.find({}))
-		// 	}
-		// },
-		methods: {
-			createTeam() {
+        // },
+        // created() {
+        // 	this.$subscribe('teams');
+        // },
+        // computed: {
+        // 	teams() {
+        // 		return this.$autorun(() => Teams.find({}))
+        // 	}
+        // },
+        methods: {
+            createTeam() {
 
-				console.log("createTeam Name and Description", this.newTeamInfo.name, this.newTeamInfo.description);
-				const { name, description } = this.newTeamInfo;
-				Meteor.call('teams.insert', this.newTeamInfo, function (error, success) {
-					if (error) {
-						console.log('error', error);
-					}
-					if (success) {
-						swal("Success", "New Team Added", "success");
-					}
-				});
-
-
+                console.log("createTeam Name and Description", this.newTeamInfo.name, this.newTeamInfo.description);
+                const {name, description} = this.newTeamInfo;
+                Meteor.call('teams.insert', this.newTeamInfo, function (error, success) {
+                    if (error) {
+                        console.log('error', error);
+                    }
+                    if (success) {
+                        swal("Success", "New Team Added", "success");
+                    }
+                });
 
 
+            }
+        }
 
-			}
-		}
-
-	}
+    }
 </script>
 
 <style>
